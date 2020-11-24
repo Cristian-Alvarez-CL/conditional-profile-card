fd59c678c76619807565a52cb2772dce8bde9ad7
CAP <cap@cap.com>
2020-11-23 21:37:15 -0300

    Se agrega condiciones IF y Ternario

--- src/js/index.js
+++ src/js/index.js
@@ -29,18 +29,75 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  variables.name === null ? (variables.name = "Lucy") : variables.name;
+  variables.lastname === null
+    ? (variables.lastname = "Boilett")
+    : variables.lastname;
+
+  variables.socialMediaPosition === null
+    ? (variables.socialMediaPosition = "position-right")
+    : variables.socialMediaPosition;
+
+  variables.twitter === null ? (variables.twitter = "#") : variables.twitter;
+
+  variables.github === null ? (variables.github = "#") : variables.github;
+
+  variables.linkedin === null ? (variables.linkedin = "#") : variables.linkedin;
+
+  variables.instagram === null
+    ? (variables.instagram = "#")
+    : variables.instagram;
+
+  if (variables.role === null || variables.role === "Web Developer") {
+    variables.role = "Web Developer";
+  } else {
+    if (variables.role === "Floor Planner") {
+      variables.role = "Floor Planner";
+    } else {
+      variables.role = "Technical Writter";
+    }
+  }
+
+  if (variables.city === null || variables.city === "Miami") {
+    variables.city = "Miami";
+  } else {
+    if (variables.city === "Munich") {
+      variables.city = "Munich";
+    } else {
+      if (variables.city === "Caracas") {
+        variables.city = "Caracas";
+      } else {
+        variables.city = "Toronto";
+      }
+    }
+  }
+
+  if (variables.country === null || variables.country === "USA") {
+    variables.country = "USA";
+  } else {
+    if (variables.country === "Germany") {
+      variables.country = "Germany";
+    } else {
+      if (variables.country === "Canada") {
+        variables.country = "Canada";
+      } else {
+        variables.country = "Venezuela";
+      }
+    }
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}" target="_blank"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}" target="_blank""><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}" target="_blank""><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
